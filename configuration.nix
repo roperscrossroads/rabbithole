c474a3b90b7539ec818824ed6026af281b61a600
Adam Roper <adam.r.roper@gmail.com>
2024-12-16 22:45:59 -0500

    fix: formatting

--- configuration.nix
+++ configuration.nix
@@ -69,8 +69,8 @@ in {
   # Supress systemd units that don't work because of LXC.
   # https://blog.xirion.net/posts/nixos-proxmox-lxc/#configurationnix-tweak
   systemd.suppressedSystemUnits = [
-    "dev-mqueue.mount",
-    "sys-kernel-debug.mount",
+    "dev-mqueue.mount"
+    "sys-kernel-debug.mount"
     "sys-fs-fuse-connections.mount"
   ];
 
